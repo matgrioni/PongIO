192b0f2c74e2eb39c5c3604f92c194b39c94ca99
Matias Grioni <matgrioni@gmail.com>
2016-03-06 10:48:51 -0500

    Chose best analysis method for cup

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import gopro
 import time
 import cv2 as cv
 
-# from matplotlib import pyplot as plt
-
-
 g = gopro.GoPro()
 g.shutter()
 
@@ -19,29 +16,14 @@ img = cv.imread('images/' + filename, 0)
 template = cv.imread('template/template.jpg', 0)
 w, h = template.shape[::-1]
 
-functions = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-           'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+res = cv.matchTemplate(img, template, cv.TM_CCOEFF_NORMED)
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
-for function in functions:
-    method = eval(function)
+top_left = max_loc
+bottom_right = (top_left[0] + w, top_left[1] + h)
 
-    res = cv.matchTemplate(img, template, method)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-
-    cv.rectangle(img, top_left, bottom_right, 255, 2)
-    #plt.subplot(121),plt.imshow(res,cmap = 'gray')
-    #plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    #plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    #plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    #plt.suptitle(meth)
-
-    #plt.show()
+cv.rectangle(img, top_left, bottom_right, 255, 2)
+print(top_left)
 
 cv.imshow('Image frame', img)
 cv.waitKey(0)
